aa185158612ea5b5199abb30477e8ed018b7d839
adroslice <a.schlett@gmx.de>
2021-02-26 23:29:21 +0100

    Added Userscript Definition

---
+++ main.ts
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         Dollaris
+// @version      0.1
+// @description  A modular event-driven userscript to improve the drrr.com experience.
+// @match        https://drrr.com/room/?id=*
+// ==/UserScript==
