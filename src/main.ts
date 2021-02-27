4563698a3c056393e1c30b8269b5d27ce33b495d
adroslice <a.schlett@gmx.de>
2021-02-27 13:52:26 +0100

    Main structure (Namespace) and non-systematic tweaks
    - NS-Tweaks will be modules later
    - A global declaration is used to assert that JQuery exists already on the page. It's competely untyped, or rather, typed as any, which maybe can be fixed down the line with official typescript declarations.

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@
 // @description  A modular event-driven userscript to improve the drrr.com experience.
 // @match        https://drrr.com/room/?id=*
 // ==/UserScript==
+
+namespace Dollaris {
+    declare var $: any;
+
+    /// Non-Systematic Tweaks
+    // DM-Count-Button
+    $('div.volume').after('<input type="button" value="Count DMs" onclick=\'$.get("/json.php?fast=1", (data)=>{ alert(35-data["talks"].length) });\' style="margin: 8px; background: transparent; border: 2px solid white; box-sizing: border-box; float: right;">');
+    // Autofocus Textbox
+    $(window).focus(() => $('textarea.form-control').focus());
+}
