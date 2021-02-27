abf9b863e3a958ffb2e267cd5c00c36e4b15577b
adroslice <a.schlett@gmx.de>
2021-02-27 17:23:04 +0100

    Added Event and Object definitions
    - TODO: Limit icon to the limited set of options

--- src/Module.ts
+++ src/Module.ts
@@ -2,3 +2,35 @@ type Module = {
     name: string;
     data?: any;
 }
+
+type UserJoinedEvent = (e: UserJoinedEventArgs) => void;
+type UserLeftEvent = (e: UserLeftEventArgs) => void;
+type RolledEvent = (e: RolledEventArgs) => void;
+type MessageReceivedEvent = (e: MessageReceivedEventArgs) => void;
+type MessageReceivedAnyEvent = (e: MessageReceivedAnyEventArgs) => void;
+
+type UserJoinedEventArgs = { id: string; time: number; user: User; }
+type UserLeftEventArgs = { id: string; time: number; user: User; }
+type RolledEventArgs = { id: string; time: number; from: User; to: User; }
+type MessageReceivedEventArgs = { message: Message; }
+type MessageReceivedAnyEventArgs = { message: Message; }
+
+type User = {
+    device: "bot" | "tv" | "tablet" | "phone" | "desktup";
+    icon: string;
+    id: string;
+    name: string;
+    tripcode?: string;
+    admin?: boolean;
+};
+
+type Message = {
+    id: string;
+    time: number;
+    type: "message" | "me" | "roll" | "join" | "leave";
+    message: string;
+
+    user?: User;
+    from?: User;
+    to?: User;
+};
